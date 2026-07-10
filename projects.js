ee26d43a08eabab74ff30f0a0368d88028daf94f
Arman Haghshenas <arm4ny@proton.me>
2026-07-10 20:54:26 +0330

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,44 @@
+const projects = [
+  { 
+    title: 'Startup Brand Identity', 
+    tag: 'Branding', 
+    img: 'https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80', 
+    link: 'https://armvnhs.ir/' 
+  },
+  { 
+    title: 'Mobile App Interface', 
+    tag: 'UI/UX', 
+    img: 'https://images.unsplash.com/photo-1618761714954-0b8cd0026356?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80', 
+    link: '#' 
+  },
+  { 
+    title: 'Food Poster', 
+    tag: 'Poster', 
+    img: 'https://images.unsplash.com/photo-1628102491629-778571d893a3?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80', 
+    link: '#' 
+  },
+  { 
+    title: 'Corporate Logo', 
+    tag: 'Branding', 
+    img: 'https://images.unsplash.com/photo-1599305445671-ac291c95aaa9?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80', 
+    link: '#' 
+  },
+  { 
+    title: 'E-commerce Website', 
+    tag: 'UI/UX', 
+    img: 'https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80', 
+    link: '#' 
+  },
+  { 
+    title: 'Coffee Box', 
+    tag: 'Poster', 
+    img: 'https://images.unsplash.com/photo-1559525839-b184a4d698c7?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80', 
+    link: '#' 
+  },
+  { 
+    title: 'Core Isfahan', 
+    tag: 'Sticker', 
+    img: 'https://images.unsplash.com/photo-1559525839-b184a4d698c7?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80', 
+    link: '#' 
+  }
+];
