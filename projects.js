92f0237a868d8347c2caa1824fea4a96df681ddc
Arman Haghshenas <arm4ny@proton.me>
2026-07-15 21:53:36 +0330

    Update projects.js

--- projects.js
+++ projects.js
@@ -5,15 +5,4 @@ const projects = [
     img: 'assets/img/Projects/Isfahan-Core.png', 
     link: 'https://www.instagram.com/p/DZxdnAiDc7x/?utm_source=ig_web_button_share_sheet&igsh=MzRlODBiNWFlZA==' 
   },
-  { 
-    title: 'Startup Brand Identity', 
-    tag: 'Sticker', 
-    img: 'assets/img/Projects/Isfahan-Core.png', 
-    link: 'https://www.instagram.com/p/DZxdnAiDc7x/?utm_source=ig_web_button_share_sheet&igsh=MzRlODBiNWFlZA==' 
-  },
-    { 
-    title: 'Startup Brand Identity', 
-    tag: 'Sticker', 
-    img: 'assets/img/Projects/Isfahan-Core.png', 
-    link: 'https://www.instagram.com/p/DZxdnAiDc7x/?utm_source=ig_web_button_share_sheet&igsh=MzRlODBiNWFlZA==' 
-  },
+];
