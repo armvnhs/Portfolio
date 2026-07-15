bda9f9196a73f172368526e142944ecb31cd527f
Arman Haghshenas <arm4ny@proton.me>
2026-07-15 21:40:43 +0330

    Update projects.js

--- projects.js
+++ projects.js
@@ -2,43 +2,6 @@ const projects = [
   { 
     title: 'Startup Brand Identity', 
     tag: 'Branding', 
-    img: 'assets/img/Projects/Post-Mahhon-Cover.jpg', 
-    link: 'https://armvnhs.ir/' 
+    img: 'assets/img/Projects/Isfahan-Core.png', 
+    link: 'https://www.instagram.com/p/DZxdnAiDc7x/?utm_source=ig_web_button_share_sheet&igsh=MzRlODBiNWFlZA==' 
   },
-  { 
-    title: 'Mobile App Interface', 
-    tag: 'UI/UX', 
-    img: 'https://images.unsplash.com/photo-1618761714954-0b8cd0026356?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80', 
-    link: '#' 
-  },
-  { 
-    title: 'Food Poster', 
-    tag: 'Poster', 
-    img: 'https://images.unsplash.com/photo-1628102491629-778571d893a3?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80', 
-    link: '#' 
-  },
-  { 
-    title: 'Corporate Logo', 
-    tag: 'Branding', 
-    img: 'https://images.unsplash.com/photo-1599305445671-ac291c95aaa9?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80', 
-    link: '#' 
-  },
-  { 
-    title: 'E-commerce Website', 
-    tag: 'UI/UX', 
-    img: 'https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80', 
-    link: '#' 
-  },
-  { 
-    title: 'Coffee Box', 
-    tag: 'Poster', 
-    img: 'https://images.unsplash.com/photo-1559525839-b184a4d698c7?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80', 
-    link: '#' 
-  },
-  { 
-    title: 'Core Isfahan', 
-    tag: 'Sticker', 
-    img: 'https://images.unsplash.com/photo-1559525839-b184a4d698c7?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80', 
-    link: '#' 
-  }
-];
