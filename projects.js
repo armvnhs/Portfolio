13847822a3c1553a260bdb154b012990a65db744
Arman Haghshenas <arm4ny@proton.me>
2026-07-15 21:40:54 +0330

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,7 +1,7 @@
 const projects = [
   { 
     title: 'Startup Brand Identity', 
-    tag: 'Branding', 
+    tag: 'Sticker', 
     img: 'assets/img/Projects/Isfahan-Core.png', 
     link: 'https://www.instagram.com/p/DZxdnAiDc7x/?utm_source=ig_web_button_share_sheet&igsh=MzRlODBiNWFlZA==' 
   },
